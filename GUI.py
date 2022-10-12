e742a442fd762c031f9d4f1fc7697b85b5b1de24
dharmisha2207 <112656508+dharmisha2207@users.noreply.github.com>
2022-10-12 11:47:24 +0530

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -9,6 +9,8 @@ class Bunny:
     def __init__(self, bunny_x, bunny_y):
         self.bunny = pygame.image.load(os.path.join('Assets','bunny.png'))
         self.bunny = pygame.transform.scale(self.bunny,(self.bunny_width,self.bunny_height))
+        self.bunny_x = bunny_x
+        self.bunny_y = bunny_y
         self.x=bunny_x
         self.y=bunny_y
     
@@ -21,11 +23,37 @@ class Bunny:
     def get_y(self):
         return self.y
     
-    def increment(self):
-        self.x += 1
+    def left_increment(self, x, y):
+        if self.x + 1 > x and self.y + 1 < y:
+            return
+        if self.x + 1 > x:
+            self.y -= 1
+        else: 
+            self.x += 1
     
-    def decrement(self):
-        self.x -=1
+    def right_increment(self, x, y):
+        if self.x - 1 < x and self.y + 1 < y:
+            return
+        if self.x - 1 < x:
+            self.y -= 1
+        else:
+            self.x -= 1
+
+    def left_decrement(self, x, y):
+        if self.x - 1 < self.bunny_x and self.y + 1 < self.bunny_y:
+            return
+        if self.y + 1 < self.bunny_y:
+            self.y += 1
+        elif self.x - 1 > self.bunny_x: 
+            self.x -= 1
+    
+    def right_decrement(self, x, y):
+        if self.x + 1 > self.bunny_x and self.y + 1 > self.bunny_y:
+            return
+        if self.y + 1 < self.bunny_y:
+            self.y += 1
+        elif self.x + 1 < self.bunny_x: 
+            self.x += 1
 
 
 class Game:
@@ -35,6 +63,8 @@ class Game:
         self.height = height
         self.bunny1 = Bunny(100,600)
         self.bunny2 = Bunny(800,600)
+        self.carrot_x = 400
+        self.carrot_y = 200
         self.bg = pygame.image.load(os.path.join('Assets','bg.jpeg'))
         self.bg = pygame.transform.scale(self.bg,(width,height))
         self.carrot = pygame.image.load(os.path.join('Assets','carrot.png'))
@@ -46,8 +76,20 @@ class Game:
         self.win.blit(self.bg, (0,0))
         self.win.blit(self.bunny1.get_bunny(),(self.bunny1.get_x(), self.bunny1.get_y()))
         self.win.blit(self.bunny2.get_bunny(),(self.bunny2.get_x(), self.bunny2.get_y()))
-        self.win.blit(self.carrot,(400,200))
+        self.win.blit(self.carrot,(self.carrot_x,self.carrot_y))
         pygame.display.update()
+
+    def player1_inc(self):
+        self.bunny1.left_increment(self.carrot_x, self.carrot_y)
+    
+    def player2_inc(self):
+        self.bunny2.right_increment(self.carrot_x, self.carrot_y)
+    
+    def player1_dec(self):
+        self.bunny1.left_decrement(self.carrot_x, self.carrot_y)
+    
+    def player2_dec(self):
+        self.bunny2.right_decrement(self.carrot_x, self.carrot_y)
 
 
     def run(self):
@@ -56,8 +98,8 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-            self.bunny1.increment()
-            self.bunny2.decrement()
+            self.player1_inc()
+            self.player2_inc()
             self.draw_window()
         pygame.quit()
 
