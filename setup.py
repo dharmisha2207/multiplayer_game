b1b49cb6f4f29ad54c8e47db83e3e63189a55407
dharmisha2207 <112656508+dharmisha2207@users.noreply.github.com>
2022-10-15 00:03:35 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='Random_Arithmetic_question',
-      version='0.0',
-      description='Random Arithmetic Question',
-      packages=['Random_Arithmetic_question'],
-      author_email='dharmisha2207@gmail.com',
-      zip_safe=False)
