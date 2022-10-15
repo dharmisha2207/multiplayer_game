85da20ac55a3d4363d2f40aa0142279c0fd949ac
Raaji-99 <104925973+Raaji-99@users.noreply.github.com>
2022-10-15 19:06:00 +0530

    Add files via upload

--- client_code.py
+++ client_code.py
@@ -8,6 +8,10 @@ import os
 from time import sleep
 import Random_Arithmetic_question
 import threading
+import pygame
+
+pygame.mixer.init()
+pygame.font.init()
 
 class client:
     def __init__(self):
@@ -21,11 +25,13 @@ class client:
 
         self.run=False
 
+        self.winner_announced=False
+
 
     def parse_question(self,ques_str):
         ques=Random_Arithmetic_question.Question_Generator()
         if ques_str[1]=='+' or ques_str[1]=='-':
-            ques.a=ques_str[0]
+            ques.a=int(ques_str[0])
             if ques_str[1]=='+':
                 ques.c=0
             else:
@@ -33,7 +39,7 @@ class client:
             if len(ques_str)==3:
                 ques.b=int(ques_str[2])
             else:
-                ques.b=int(ques_str[2]*10+ques_str[3])
+                ques.b=int(ques_str[2])*10+int(ques_str[3])
 
         else:
             ques.a=int(ques_str[0])*10+int(ques_str[1])
@@ -44,14 +50,12 @@ class client:
             if len(ques_str)==4:
                 ques.b=int(ques_str[3])
             else:
-                ques.b=int(ques_str[3]*10+ques_str[4])
+                ques.b=int(ques_str[3])*10+int(ques_str[4])
 
+        ques.current_ques = str(ques.a) + ques.ops[ques.c] + str(ques.b)
         return ques
 
-    def draw_GUI(self):
-        while self.run:
-            self.game.draw_window()
-            sleep(2)
+
 
 
     def arrival(self):
@@ -59,38 +63,43 @@ class client:
         msg = self.net.receive()
         if msg == "Arrived":
             print("Everyone Arrived")
+            self.run=True
         else:
             print("Error")
             exit(-1)
 
     def driver_code(self):
 
-
-        arrival = threading.Thread(target=self.arrival)
-        arrival.start()
-
         if (self.id == 1):
             # Wait indefinitely till server informs of arrival of all participants
+            arrival = threading.Thread(target=self.arrival)
+            arrival.start()
+
             while not self.run:
                 self.game.draw_waiting()
                 sleep(1)
 
+            arrival.join()
+
         self.game.toggle_run()
         self.run=True
 
-        #Draw Game world
-        if self.id==1:
-            arrival.join()
 
-        drawing_thread = threading.Thread(target=self.draw_GUI, args=())
-        drawing_thread.start()
+        round= threading.Thread(target=self.Round)
+        round.start()
 
+        while self.run:
+            self.game.draw_window()
+
+
+
+    def Round(self):
         while True:
             #wait for question from server
             question_str = self.net.receive()
             question=self.parse_question(question_str)
 
-            print("question received: "+question_str)
+            print("question received: "+question.current_ques)
 
             self.game.update_ques(question)
 
@@ -103,11 +112,53 @@ class client:
             #check for correct answer
             #if not correct continue loop
             #if correct send id to server and break loop
+
+            correct=False
+
             if self.game.play():
                 self.net.send(self.id)
 
-            winner_announce.join()
+            #while self.run:
+            #    if self.winner_announced:
+            #        self.winner_announced = False
+            #        break
+            #    winner_id=int(self.net.try_receive())
+            #    if winner_id!= -1:
+            #        self.winner_announced=True
+            #        if winner_id == 1:
+            #            self.game.player1_inc()
+            #            self.game.player2_dec()
+            #        else:
+            #            self.game.player2_inc()
+            #            self.game.player1_dec()
+            #    break
+            #    if not correct:
+            #        for event in pygame.event.get():
+            #            if event.type == pygame.QUIT:
+            #                self.run = False
+            #            if event.type == pygame.KEYDOWN:
+            #                print("key")
+            #                if event.key == pygame.K_BACKSPACE:
+            #                    self.game.answer = self.game.answer[:-1]
+            #                    self.game.update_text()
+            #                else:
+            #                    self.game.answer += event.unicode
+            #                    self.game.update_text()
+            #                if event.key == pygame.K_RETURN:
+            #                    if int(self.game.answer) == question.answer():
+            #                        self.game.answer = ''
+            #                        self.game.update_text()
+            #                        self.net.send(self.id)
+            #                        correct=True
+            #                        break
+            #                    else:
+            #                        self.game.wrong_sound.play()
+            #                        self.game.answer = ''
+            #                        self.game.update_text()
+            #        pygame.display.update()
+            #pygame.quit()
 
+            winner_announce.join()
 
     def winner_receive(self):
 
